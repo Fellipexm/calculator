49ddd7f649d67b2d46be295984ae4ec518797546
Fellipe Ribeiro Martins <121965162+Fellipedev1@users.noreply.github.com>
2024-03-23 00:18:30 -0300

    comint inicial

---
+++ calculator.py
@@ -0,0 +1,18 @@
+primeiro =float(input("Digite o primeiro numero: "))
+
+Segundo  = float(input("Digite o segundo numero: "))
+
+calculo = str(input("Escolha sua operaçao, +, -, *, /: "))
+
+if calculo == '+':
+  print (primeiro + Segundo)
+  
+elif calculo == '-':
+    print(primeiro - Segundo)
+
+elif calculo == '*':
+    print(primeiro / Segundo)
+    
+elif calculo == '/':
+    print(primeiro / Segundo)
+    
